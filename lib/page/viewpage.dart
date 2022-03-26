3df0195c681f9f4805a1159110d5f85fd52bfb23
Eugene Debrah <debraheug@gmail.com>
2022-03-26 18:25:39 +0000

    AppBar, ViewPage

--- lib/page/viewpage.dart
+++ lib/page/viewpage.dart
@@ -11,7 +11,25 @@ class _ViewPageState extends State<ViewPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      color: const Color(0x0fffffff),
+      child: Stack(
+        children: [
+          ListView(
+            physics: const BouncingScrollPhysics(),
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(20, 56, 20, 0),
+                    child: ,
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/appBar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppBarTitle extends StatelessWidget {
+  final String title;
+  final String subtiitle;
+  final String profileImage;
+
+  // ignore: use_key_in_widget_constructors
+  const AppBarTitle(this.title, {this.subtiitle = "", required this.profileImage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
