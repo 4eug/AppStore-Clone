f00c3276e862951d9673bf74dba923e8ac19ced3
Eugene Debrah <debraheug@gmail.com>
2022-03-26 17:41:08 +0000

    Colors Added

---
+++ lib/style/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const Color primaryColor = Color(0xffffffff);
+  static const Color primaryLightColor = Color(0xff99989d);
+  static const Color primaryDarkLightColor = Color(0xffc5c5c7);
+  static const Color primaryDarkColor = Color(0xff000000);
+  static const Color accentColor = Color(0xff0474fa);
+  static const Color transparentColor = Color(0x00000000);
+}
