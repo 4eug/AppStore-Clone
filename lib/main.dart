8fac203d7fb54de6e3da2c7a08d455bfc33ee4de
Eugene Debrah <debraheug@gmail.com>
2022-03-26 17:39:01 +0000

    Lets Start:- Structure Setup

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:appstore_clone/page/viewpage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: ViewPage(),
+    );
+  }
+}
+
+
+ 
---
+++ lib/page/viewpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ViewPage extends StatefulWidget {
+  const ViewPage({ Key? key }) : super(key: key);
+
+  @override
+  State<ViewPage> createState() => _ViewPageState();
+}
+
+class _ViewPageState extends State<ViewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
