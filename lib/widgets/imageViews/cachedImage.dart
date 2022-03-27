90d4609eaf6bd834d0bc738434bc6b4dd63f9152
Eugene Debrah <debraheug@gmail.com>
2022-03-27 06:58:41 +0000

    Cached Network Image

---
+++ lib/widgets/imageViews/cachedImage.dart
@@ -0,0 +1,29 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class CachedImage extends StatelessWidget {
+  const CachedImage(this.url);
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+        placeholder: (context, url) => SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Shimmer.fromColors(
+            baseColor: Colors.grey,
+            highlightColor: Colors.grey,
+            child: Container(
+              decoration: const BoxDecoration(
+                  shape: BoxShape.rectangle, color: Colors.white),
+            ),
+          ),
+        ),
+        imageUrl: url,
+        width: double.infinity,
+        height: double.infinity,
+        fit: BoxFit.cover);
+  }
+}
