eaca913d252167641ca9306e9ce5ff77c6c62481
Eugene Debrah <debraheug@gmail.com>
2022-03-27 09:51:22 +0000

    BottomNavBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:appstore_clone/page/viewpage.dart';
 import 'package:appstore_clone/style/colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,9 +28,54 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       backgroundColor: ColorStyles.primaryDarkColor,
-      body: ViewPage(),
+      bottomNavigationBar:DecoratedBox(
+        decoration: const BoxDecoration(
+          color: Colors.purple),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(1, 5, 1, 5),
+            child: SafeArea(
+              top: false,
+              // ignore: unnecessary_const
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.home),
+                    color: Colors.white,
+                    iconSize: 30,
+                    onPressed: (){},
+                    ),
+                IconButton(
+                  icon: const Icon(CupertinoIcons.search),
+                  color: Colors.white,
+                  iconSize: 30,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(CupertinoIcons.bell),
+                  color: Colors.white,
+                  iconSize: 30,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: const Icon(CupertinoIcons.person),
+                  color: Colors.white,
+                  iconSize: 30,
+                  onPressed: () {},
+                ),
+                 IconButton(
+                  icon: const Icon(CupertinoIcons.person),
+                  color: Colors.white,
+                  iconSize: 30,
+                  onPressed: () {},
+                ), 
+                ],
+              )),
+          ),
+          ) ,
+      body: const ViewPage(),
     );
   }
 }
