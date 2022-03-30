22df7cd17982e311f5841dc12bc281337111abcd
Eugene Debrah <debraheug@gmail.com>
2022-03-30 09:41:34 +0000

    added Subdescription

--- lib/models/app_model.dart
+++ lib/models/app_model.dart
@@ -2,6 +2,7 @@ class AppInfo {
   final String thumbnailUrl;
   final String name;
   final String description;
+  final String subdescription;
 
-  const AppInfo(this.thumbnailUrl, this.name, this.description);
+  const AppInfo(this.thumbnailUrl, this.name, this.description, this.subdescription);
 }
