896335301294e853836e4475bcac2f051069db3a
Eugene Debrah <debraheug@gmail.com>
2022-03-30 09:43:09 +0000

    added Subdescription

--- lib/widgets/todayListView.dart
+++ lib/widgets/todayListView.dart
@@ -20,11 +20,12 @@ class TodayListState extends State<TodayListView> {
         'https://lh3.googleusercontent.com/gsbOVolF0SetvVXYh8rSSDjW5pFwx4F40dg20Hhj6617fI6W-q5aXwZxIMbR2cYJmgYRZV8z8Egzz4-quVZc3WZdH3pL7bScl5LNj1sqmtJNWD3xQbzRDkj0V5SZdPFA68XX7qsO',
         'Mad Hot \nLava',
         AppInfo(
-            'https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/7c/d7/66/7cd766cf-c775-08e1-49ae-2d5611376450/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/246x0w.png',
-            'Hot Lava',
-            'Exclusive Game')),
+            'https://sportsnews.page/wp-content/uploads/2021/04/pubg-512x416.png',
+            'PUBG MOBILE',
+            '#Top Battle Royale ',
+            'Mobile Game')),
     const PlainToday(
-        'https://images.unsplash.com/photo-1592651595884-957a8f9c1d52?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        'https://images.unsplash.com/photo-1584284807530-cb3c3248adc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80',
         'Mid-Night',
         'Photograhy',
         description: 'Snap Beautiful Pictures'),
@@ -34,7 +35,9 @@ class TodayListState extends State<TodayListView> {
         AppInfo(
             'https://is1-ssl.mzstatic.com/image/thumb/Purple113/v4/6d/9d/59/6d9d5931-e300-fa8f-df4f-0087932d0989/AppIcon-1x_U007emarketing-0-0-GLES2_U002c0-512MB-sRGB-0-0-0-85-220-0-0-0-6.png/400x400.png',
             'Fortnite',
-            'Exclusive Game'))
+            'Exclusive Game',
+            'Mobile Game'
+            ))
   ];
 
   @override
